29fbb0c7839e0189921ed1b54c5eee928c21199c
Adam Kowalczewski <statscancancancan@gmail.com>
2023-04-12 08:18:22 -0400

    improved searcher behaviour

--- script.js
+++ script.js
@@ -90,7 +90,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const avalancheGif = document.getElementById("avalanche-gif");
     const avalancheText = document.getElementById("avalanche-text");
 
-  // Add this function to show the "Let's ride" button and hide the forecast-choice section
   function showLetsRide() {
       document.querySelector(".forecast-choice").style.display = "none";
       document.getElementById("lets-ride").style.display = "block";
@@ -204,11 +203,11 @@ document.addEventListener("DOMContentLoaded", function () {
     
   // Create searchers array
   const searchers = [
-    { x: 150, y: 150, color: "#009E73", proximity: "Search for signal" },
-    { x: 160, y: 160, color: "#56B4E9", proximity: "Search for signal" },
-    { x: 170, y: 170, color: "#E69F00", proximity: "Search for signal" },
+    { x: 150, y: 150, color: "#009E73", proximity: "Search for signal", displayText: "Search for signal", speed: 1, randomness: 0.3 },
+    { x: 160, y: 160, color: "#56B4E9", proximity: "Search for signal", displayText: "Search for signal", speed: 1.5, randomness: 0.5 },
+    { x: 500, y: 170, color: "#E69F00", proximity: "Search for signal", displayText: "Search for signal", speed: 1.5, randomness: 0.7 },
   ];
-  
+    
   function isInsideTriangle(px, py) {
     const x1 = 100, y1 = 350, x2 = 300, y2 = 50, x3 = 500, y3 = 350;
   
@@ -222,19 +221,35 @@ document.addEventListener("DOMContentLoaded", function () {
   
 
   function drawSearchers() {
-    searchers.forEach((searcher) => {
-      ctx.fillStyle = searcher.color;
+    ctx.clearRect(0, 0, mountainCanvas.width, mountainCanvas.height);
+  
+    searchers.forEach((searcher, index) => {
       ctx.beginPath();
       ctx.arc(searcher.x, searcher.y, 5, 0, 2 * Math.PI);
+      ctx.fillStyle = searcher.color;
       ctx.fill();
+      ctx.closePath();
   
-      if (searcher.proximity !== "Search for signal") {
-        ctx.font = "14px Arial";
-        ctx.fillStyle = "black"; // Set fillStyle for the text
-        ctx.fillText(searcher.proximity, searcher.x + 10, searcher.y - 10);
-        ctx.fillStyle = searcher.color; // Reset fillStyle to the original value
-      }
+      ctx.font = "12px Arial";
+      ctx.fillStyle = searcher.color;
+      ctx.fillText(searcher.displayText, searcher.x + 10, searcher.y + 5 + (index * 14));
     });
+  }
+    
+
+  function checkForProbeStrikes() {
+    const probingSearcher = searchers.find(searcher => searcher.state === "Probe Strike");
+  
+    if (probingSearcher) {
+      searchers.forEach((helper) => {
+        if (helper !== probingSearcher && !helper.isHelping && !helper.isProbing) {
+          helper.isHelping = true;
+          helper.proximity = "Helping";
+          helper.x += Math.sign(probingSearcher.x - helper.x) * 10;
+          helper.y += Math.sign(probingSearcher.y - helper.y) * 10;
+        }
+      });
+    }
   }
   
 
@@ -248,52 +263,71 @@ document.addEventListener("DOMContentLoaded", function () {
         // Check for proximity and update
         const distance = calculateDistance(searcher.x, searcher.y, victimX, victimY);
         const proximity = Math.round(distance / (mountainCanvas.width / 4 / 40));
-        searcher.proximity = proximity <= 40 ? proximity : "Search for signal";
   
+        if (!searcher.isProbing && !searcher.isHelping) {
+            searcher.proximity = proximity <= 40 ? proximity : "Search for signal";
+            searcher.displayText = searcher.proximity;
+          } else if (searcher.state === "Probing") {
+            searcher.displayText = "Probing...!";
+          } else if (searcher.state === "Probe Strike") {
+            searcher.displayText = "Probe Strike!";
+          } else if (searcher.state === "Digging") {
+            searcher.displayText = "Digging...";
+          } else if (searcher.isHelping) {
+            searcher.displayText = "Helping";
+        }
+          
         if (searcher.proximity <= 5 && !searcher.isProbing && !searcher.isHelping) {
-          searcher.isProbing = true;
-          searcher.proximity = "Probing...!";
+
           setTimeout(() => {
-            searcher.proximity = "Probe Strike!";
-            searcher.x = victimX;
-            searcher.y = victimY;
-  
-            setTimeout(() => {
-              searcher.proximity = "Digging...";
-              searchers.forEach((helper) => {
-                if (helper !== searcher && !helper.isHelping) {
-                  const distanceToSearcher = calculateDistance(helper.x, helper.y, searcher.x, searcher.y);
-                  if (distanceToSearcher <= 10) {
+            // Add a 25% chance of probe failure
+            if (Math.random() < 0.25) {
+              searcher.isProbing = false;
+              searcher.proximity = "Probe failed";
+              setTimeout(() => {
+                searcher.proximity = "Searching...";
+              }, 1000);
+            } else {
+              searcher.state = "Probe Strike";
+              searcher.proximity = "Probe Strike!";
+              searcher.x = victimX;
+              searcher.y = victimY;
+          
+              setTimeout(() => {
+                searcher.state = "Digging";
+                searcher.proximity = "Digging...";
+                searchers.forEach((helper) => {
+                  if (helper !== searcher && !helper.isHelping && !helper.isProbing) {
                     helper.isHelping = true;
                     helper.proximity = "Helping";
                     helper.x += Math.sign(searcher.x - helper.x) * 10;
                     helper.y += Math.sign(searcher.y - helper.y) * 10;
                   }
-                }
-              });
-  
-            }, 3000);
-  
+                });
+              }, 3000);
+
+            }
           }, Math.floor(Math.random() * 5000) + 1000);
-  
-        } else if (!searcher.isProbing && !searcher.isHelping) {
           
+          
+        }  else if (!searcher.isProbing && !searcher.isHelping) {
           const randomPercentage = Math.random() * 100;
           const angleToVictim = Math.atan2(victimY - searcher.y, victimX - searcher.x);
           const arcAngle = (Math.PI / 180) * 45; // 45 degrees in radians
-        
+  
+          const randomFactorX = searcher.randomness * (Math.random() - 0.5);
+          const randomFactorY = searcher.randomness * (Math.random() - 0.5);
+  
           if (randomPercentage < 85) {
             // Move in an arc towards the victim
-            searcher.x += Math.cos(angleToVictim + arcAngle) * speed;
-            searcher.y += Math.sin(angleToVictim + arcAngle) * speed;
+            searcher.x += (searcher.speed * Math.cos(angleToVictim + arcAngle)) + randomFactorX;
+            searcher.y += (searcher.speed * Math.sin(angleToVictim + arcAngle)) + randomFactorY;
           } else {
             // 15% chance of moving in the wrong direction
-            searcher.x += Math.cos(angle + Math.PI) * speed;
-            searcher.y += Math.sin(angle + Math.PI) * speed;
+            searcher.x += (searcher.speed * Math.cos(angle + Math.PI)) + randomFactorX;
+            searcher.y += (searcher.speed * Math.sin(angle + Math.PI)) + randomFactorY;
           }
         }
-        
-
       } else {
         
         // Signal search
@@ -317,8 +351,10 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(() => {
     clearCanvas();
     moveSearchers();
+    checkForProbeStrikes();
     drawSearchers();
     drawMountain();
   }, 1000);
+  
 
 });
